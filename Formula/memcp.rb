2cc4ab38647d5c29bdfc4bd6ab105f528df38e50
Tommy Maintz <tommy.maintz@gmail.com>
2026-02-13 21:20:26 +0100

    memcp: add license field

--- Formula/memcp.rb
+++ Formula/memcp.rb
@@ -1,6 +1,7 @@
 class Memcp < Formula
   desc "Cross-session persistent memory MCP server for coding agents"
   homepage "https://github.com/helixerio/memcp"
+  license "MIT"
   url "https://github.com/helixerio/memcp/archive/refs/tags/v0.1.0.tar.gz",
     header: "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
   sha256 "9fb1a5eda1f13030bb2ecceb27e0b188096d014b1ce3a1ec2e24092e0b77e639"
