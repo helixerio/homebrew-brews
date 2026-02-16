9f1164ce69aa7e854b0edb386a597463c4f26d14
Sander van Harmelen <sander@vanharmelen.nl>
2026-02-16 11:43:11 +0100

    Rebuild and publish depot bottles

--- Formula/depot.rb
+++ Formula/depot.rb
@@ -5,14 +5,6 @@ class Depot < Formula
     header: "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
   sha256 "de852c5f86d9eaec8664fda9e00be1e3f484dae4effca4600721b155d0bc2945"
 
-  bottle do
-    root_url "https://github.com/helixerio/homebrew-brews/releases/download/depot-0.5.0"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "029825e314cf68af3cb7effd1da966d1fdb021d18c9cec6635fc34350e76350a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3c92697dab73001da6888295230a4d2694f1e93db10ca8ca989699626e0d4222"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "730486b63ae77fb82ff990ca1f0b43b02ccaee7145f650412b3a359c5da613a1"
-  end
-
   depends_on "go" => :build
 
   def install
