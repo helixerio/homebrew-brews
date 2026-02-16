e5958c0a4ed53c5b592470a896558c058df01c43
Sander van Harmelen <sander@vanharmelen.nl>
2026-02-16 12:00:15 +0100

    Update depot to 0.5.0

--- Formula/depot.rb
+++ Formula/depot.rb
@@ -1,17 +1,19 @@
 class Depot < Formula
   desc "Private Terraform Registry with support for Modules and Providers"
   homepage "https://github.com/helixerio/depot"
-  url "https://github.com/helixerio/depot/archive/refs/tags/v0.3.1.tar.gz",
+  url "https://github.com/helixerio/depot/archive/refs/tags/v0.5.0.tar.gz",
     header: "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
-  sha256 "4fe26ca0aa59cebaea45a2c12a0672b365ddd5bc149fa00241cdde2af27f48b9"
+  sha256 "de852c5f86d9eaec8664fda9e00be1e3f484dae4effca4600721b155d0bc2945"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    ENV["GOTOOLCHAIN"] = "auto"
+    ldflags = "-s -w -X github.com/helixerio/depot/cmd.currentVersion=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
   end
 
   test do
-    assert_match "Depot version: 0.3.1", shell_output("#{bin}/depot version")
+    assert_match "Depot version: #{version}", shell_output("#{bin}/depot version")
   end
 end
