6034fc240d8f7673f35def52a8bb7228a1fbd887
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2026-02-16 11:07:44 +0000

    depot: add 0.5.0 bottle.

--- Formula/depot.rb
+++ Formula/depot.rb
@@ -5,6 +5,13 @@ class Depot < Formula
     header: "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
   sha256 "de852c5f86d9eaec8664fda9e00be1e3f484dae4effca4600721b155d0bc2945"
 
+  bottle do
+    root_url "https://github.com/helixerio/homebrew-brews/releases/download/depot-0.5.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4f1662f190b41813fabee8cb54acc660d4c67f98d9ca82bcef7ca6abef352992"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "87a1ef605d8740ab77e4358372de63084a1d1d9cc538de69659806dc2eec2ffb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64e16450e695d520462c1176a06d0b1b8e83770dcf0181d2db4bde958a5c2dd6"
+  end
+
   depends_on "go" => :build
 
   def install
