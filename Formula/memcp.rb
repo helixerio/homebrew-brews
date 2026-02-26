101ed9b6771ab40519bd48af0fc5a8cf576574cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2026-02-26 08:50:30 +0000

    memcp: add 1.3.1 bottle.

--- Formula/memcp.rb
+++ Formula/memcp.rb
@@ -5,6 +5,13 @@ class Memcp < Formula
     header: "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
   sha256 "51726d0491efcc7743afd62dcdb1796d2fad5223cf90f7b13645556afaa4de44"
 
+  bottle do
+    root_url "https://github.com/helixerio/homebrew-brews/releases/download/memcp-1.3.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d857d0f918deb4ddc248fdaa3b5a9b4581ceb48be28eb82c76b9855e79fe7d95"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0bf9d0c3bb492d627d34c443fea95ea9e7f296a788b5738bce5039801d1c2bbe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e13e37f85c9f3666681f50737ca34b30d0fe52f020b7a7902eff0f59c437f52"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
 
