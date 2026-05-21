c258734722fd0c03894c578e6da9ada545684299
Tommy Maintz <tommy.maintz@gmail.com>
2026-05-21 17:37:20 +0200

    memcp: update 1.5.2
    Closes #29.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/memcp.rb
+++ Formula/memcp.rb
@@ -1,16 +1,9 @@
 class Memcp < Formula
   desc "Cross-session persistent memory MCP server for coding agents"
   homepage "https://github.com/helixerio/memcp"
-  url "https://github.com/helixerio/memcp/archive/refs/tags/v1.5.1.tar.gz",
+  url "https://github.com/helixerio/memcp/archive/refs/tags/v1.5.2.tar.gz",
     header: "Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
-  sha256 "15c79f7555aaf43b3f2d171a8706e89f07e75b10aaaf5ad257c41c8cbead1baa"
-
-  bottle do
-    root_url "https://github.com/helixerio/homebrew-brews/releases/download/memcp-1.5.1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "317ba248e4b3b8f39601e0b42bf1bfab5f5da5539b37013b2cca637902878d14"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c183ab06e94984862a5a4978759262e1f4f6d47a840817df0cbeb42d45d03ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d1256ee3d2e27f98c4a024d138241878558c09cb32f5b260faf296e501236c6"
-  end
+  sha256 "769f924b0df93ac9708813e3e66f356526c9f4a3f42927ac13f68f3fc1c4cc8b"
 
   depends_on "go" => :build
   depends_on "node" => :build
